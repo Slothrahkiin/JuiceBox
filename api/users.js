2c61f7b0a99a40abc28d45f43f99daeb4380d7c2
Allen Bass <allenbass@Allens-iMac.local>
2022-09-29 21:16:13 -0400

    part 2b

--- api/users.js
+++ api/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const { getUserByUsername } = require('../db');
+const { getUserByUsername, createUser } = require('../db');
 const usersRouter = express.Router();
 
 
@@ -47,6 +47,41 @@ usersRouter.get('/', (req, res) => {
       console.log(error);
       next(error);
     }
+  });
+  usersRouter.post('/register', async (req, res, next) => {
+    const { username, password, name, location } = req.body;
+  
+    try {
+      const _user = await getUserByUsername(username);
+  
+      if (_user) {
+        next({
+          name: 'UserExistsError',
+          message: 'A user by that username already exists'
+        });
+      }
+  
+      const user = await createUser({
+        username,
+        password,
+        name,
+        location,
+      });
+  
+      const token = jwt.sign({ 
+        id: user.id, 
+        username
+      }, process.env.JWT_SECRET, {
+        expiresIn: '1w'
+      });
+  
+      res.send({ 
+        message: "thank you for signing up",
+        token 
+      });
+    } catch ({ name, message }) {
+      next({ name, message })
+    } 
   });
 
 module.exports = usersRouter;
